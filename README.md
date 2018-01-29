2d7e565cb3da2e5bc2360afed5c50da908ad8220
Ishtmeet Singh <ish.rissam@gmail.com>
2018-01-29 23:57:03 +0530

    finalized

---
+++ README.md
@@ -0,0 +1,6 @@
+Created with create-react-app starter.
+
+==Download the entire repository==
+
+1) npm install
+2) npm start
---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+
+import { convertToRaw, EditorState, convertFromHTML, ContentState } from 'draft-js';
+import draftToHtml from 'draftjs-to-html';
+
+import fetch from 'isomorphic-fetch';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      html: '',
+      editorState: EditorState.createEmpty()
+    };
+    this.onEditorStateChange = this.onEditorStateChange.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('sample.html')
+      .then(response => response.text())
+      .then(html => {
+        // Convert HTML to editor state
+        console.log(html);
+        const blocksFromHTML = convertFromHTML(html);
+        const state = ContentState.createFromBlockArray(
+          blocksFromHTML.contentBlocks,
+          blocksFromHTML.entityMap
+        );
+        return EditorState.createWithContent(state);
+      })
+      .then(editorState => this.setState({ editorState }));
+  }
+
+  onEditorStateChange(editorState) {
+    this.setState({ editorState });
+  }
+
+  render() {
+    const raw = convertToRaw(this.state.editorState.getCurrentContent());
+    const html = draftToHtml(raw);
+    const json = JSON.stringify(raw);
+
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h4>WYSIWYG Editor using Draft.js for DoSelect</h4>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col m6">
+              <div className="card-panel">
+                <Editor
+                  editorState={ this.state.editorState }
+                  placeholder="Put your content here..."
+                  onEditorStateChange={ this.onEditorStateChange }
+                  />
+              </div>
+            </div>
+            <div className="col m6">
+              <div className="card-panel grey lighten-4">
+                <h5>HTML:</h5>
+                <pre>{ html }</pre>
+              </div>
+
+              <div className="card-panel grey lighten-2">
+                <h5>Editor state (JSON):</h5>
+                <pre>{ json }</pre>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
